39542536cbaf1b7a93b663ddf26d34f26c3db2f2
areeb <areeb@192.168.2.67>
2021-01-28 11:27:42 -0500

    put the array back at 2

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -17,7 +17,7 @@ public class StudentList {
      */
     public static void main(String[] args) {
         
-        Student[] students = new Student[3];
+        Student[] students = new Student[2];
         Scanner a = new Scanner(System.in);
         for (int i =0;i<students.length;i++){
             System.out.print("Print a student name");
