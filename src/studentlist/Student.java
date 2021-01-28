35c0362c8d84d509be674ad1dcae8c3d2befdbb6
areebtayyub <78161546+areebtayyub@users.noreply.github.com>
2021-01-28 11:36:27 -0500

    Added the date

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package studentlist;
 
 /**
  *
- * @author areeb
+ * @author areeb January 2021
  */
 public class Student {
     
